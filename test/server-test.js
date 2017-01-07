b3021c9aea94e928b5adc8b1ad7954f93de71614
shaallfar <shaallfar@gmail.com>
2017-01-06 23:12:02 -0800

    travis coverall fix cont.

--- test/server-test.js
+++ test/server-test.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 
 const server = require('../server.js');
-console.log('aaaaaaaaaaaaaaaaaaaaaaaa',server);
+
 describe('Server Test', function(){
   it('should return is running', done => {
     expect(server.isRunning).to.equal(true);
