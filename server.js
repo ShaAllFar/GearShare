546002a3d63ab75cbc3dd5f3f3267b26293c7150
shaallfar <shaallfar@gmail.com>
2017-01-06 22:40:15 -0800

    initial commit, creation of staging branch, create project file structure

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const debug = require('debug')('gear-share:server.js');
+
+const errors = require('./lib/err-middleware.js');
+
+dotenv.load();
+//something funny happening with process.env.PORT
+//hard coded to get project going
+const PORT = 3000;
+const app = express();
+
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(errors)
+
+const server = module.exports = app.listen(PORT, () => {
+  debug(`Server up: ${PORT}`);
+});
+
+server.isRunning = true;
