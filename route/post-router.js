7b678fe3b79253bd23778dd7a7e00510753a3540
Daniel Becker <dbecker4130@gmail.com>
2017-01-09 16:57:07 -0800

    post schema and post router complete

---
+++ route/post-router.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
+const debug = require('debug')('gear-share:post-router');
+
+const Post = require('../model/post.js');
+// const Gallery = require('../model/gallery.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
+
+const postRouter = module.exports = Router();
+
+postRouter.post('/api/gallery/post', bearerAuth, jsonParser, function(req ,res, next){
+  debug('POST: /api/gallery/post');
+
+  req.body.userID = req.user._id;
+  new Post(req.body).save()
+  .then(post => {
+    if(!req.body.name) return next(createError(400, 'body required'));
+    res.json(post);
+  })
+  .catch(next);
+});
+
+postRouter.get('/api/gallery/:postID', bearerAuth, function(req, res, next){
+  debug('GET: /api/gallery/:postID');
+
+  Post.findById(req.params.id)
+  .then(post => {
+    if(req.body.name === null) return next(createError(404, 'post not found'));
+    if(post.userID.toString() !== req.user._id.toString()){
+      return next(createError(401, 'invalid user'));
+    }
+    res.json(post);
+  })
+  .catch(next);
+});
+
+postRouter.put('/api/gallery/:postID', bearerAuth, jsonParser, function(req,res,next){
+  debug('PUT: /ap/gallery/:postID');
+
+  if(!req.body.name) return next(createError(400, 'body required'));
+
+  Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(post => {
+    if(post === null) return next(createError(404,'post not found'));
+    res.json(post);
+  })
+  .catch(err => next(createError(404, err.message)));
+});
+
+postRouter.delete('/api/post/:id',bearerAuth, function(req, res, next){
+  debug('DELETE: /api/post/:id');
+
+  Post.findByIdAndRemove(req.params.id)
+  .then(() => res.status(204).send())
+  .catch(err => next(createError(404, err.message)));
+});
