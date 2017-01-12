5dbd6c661bdcc7f528aff28a3717c18025b7f149
shaallfar <shaallfar@gmail.com>
2017-01-12 15:30:15 -0800

    fixed imag breaking app

--- route/auth-router.js
+++ route/auth-router.js
@@ -7,7 +7,6 @@ const basicAuth = require('../lib/basic-auth-middleware.js');
 
 const createError = require('http-errors');
 const User = require('../model/user.js');
-const image = require('../test/data/tester.png');
 
 const authRouter = module.exports = Router();
 
@@ -32,7 +31,7 @@ authRouter.post('/api/signup', jsonParser, function(req, res, next) {
 });
 authRouter.get('/', function(req,res,next){
   debug('GET: /');
-  res.sendFile(image);
+  res.send('Gear Share');
 });
 
 authRouter.get('/api/signin', basicAuth, function(req, res, next) {
