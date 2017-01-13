6f8915525b53e4d6502823f5fb9da97c674bf59a
Erick Mock <erick.f.mock@gmail.com>
2017-01-12 18:21:54 -0800

    Revert "Staging"

--- test/server-test.js
+++ test/server-test.js
@@ -4,16 +4,8 @@ const expect = require('chai').expect;
 
 const mongoose = require('mongoose');
 const server = require('../server.js');
-const serverToggle = require('./lib/toggle-server.js');
 
 describe('Server Test', function(){
-  before(done => {
-    serverToggle.serverOn(server,done);
-  });
-  after(done => {
-    serverToggle.serverOff(server,done);
-  })
-
   it('should return is running', done => {
     expect(server.isRunning).to.equal(true);
     done();
@@ -22,7 +14,7 @@ describe('Server Test', function(){
 
 describe('Database Test', function(){
   it('should return connection', done => {
-    expect(mongoose.connection.port).to.be.a('number');
+    expect(mongoose.connection.port).to.equal(57278);
     expect(mongoose.connection.name).to.equal('gear-share');
     done();
   });
