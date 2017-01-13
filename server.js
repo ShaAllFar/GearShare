9d557c2014eea939a823e8a722b1ec8531ad13cd
shaallfar <shaallfar@gmail.com>
2017-01-12 17:06:33 -0800

    as

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('dev'));
-
+//aqdadw
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/img/gear-share-logo.png');
 });
